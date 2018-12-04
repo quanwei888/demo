bb62fb464f3002b48a4ccafd67abf8f6907f62bd
quanwei888 <quanwei888@gmail.com>
2018-12-04 08:27:33 +0800

    sdfsdf

--- app/build/generated/source/apt/debug/com/lucky/demo/BR.java
+++
@@ -1,8 +0,0 @@
-package com.lucky.demo;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int book = 1;
-        public static final int data = 2;
-        public static final int statInfo = 3;
-}
--- app/build/generated/source/apt/release/com/lucky/demo/BR.java
+++
@@ -1,9 +0,0 @@
-package com.lucky.demo;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int book = 1;
-        public static final int data = 2;
-        public static final int statInfo = 3;
-        public static final int word = 4;
-}
